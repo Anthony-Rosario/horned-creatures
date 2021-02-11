be774393619e449c977b6d88392c60f61d92fdef
Anthony Rosario <anthonymrosario225@gmail.com>
2021-02-10 20:03:36 -0800

    finished filtering

--- src/App.js
+++ src/App.js
@@ -7,21 +7,61 @@ import './App.css';
 
 export default class App extends Component {
   state ={ 
-    filter: 'All'
+    keyword: '',
+    horns: 0,
   }
 
-  handlerClick = e => {
+  handlerClickImage = e => {
     this.setState({
-      filter: e.target.value
+      keyword: e.target.value
     });
   }
 
+  handlerClickHorns = e => {
+    this.setState({
+      horns: Number(e.target.value)
+    })
+  }
+
+  imageKeywords = Array.from(new Set(images.map(image => {
+    Object.keys(image);
+    return image.keyword
+  })));
+  imageHorns = Array.from(new Set(images.map(image => {
+    Object.keys(image);
+    return image.horns
+  })));
+
   render() {
+    const filteredImages=images.filter((image) => {
+      if(!this.state.keyword && !this.state.horns) return true;
+      if(this.state.keyword && !this.state.horns) {
+        if(image.keyword === this.state.keyword) return true;
+      }
+      if(this.state.horns && !this.state.keyword) {
+        if(image.horns === this.state.horns) return true;
+      }
+      if(this.state.horns && this.state.keyword) {
+        if(image.horns === this.state.horns && image.keyword === this.state.keyword) return true
+      }
+      return false
+    });
+
     return (
     <>
       <HeaderRender />
-      <DropDown handlerClick={this.handlerClick} />
-      <ImageList images={images} filter={this.state.filter}/>
+      <DropDown
+      currentValue={this.state.keyword} 
+      handlerClick={this.handlerClickImage}
+      options={this.imageKeywords} 
+      />
+      <DropDown
+
+      currentValue={this.state.horns}
+      handlerClick={this.handlerClickHorns} 
+      options={this.imageHorns}
+      />
+      <ImageList filteredImages={filteredImages} />
     </>
     )
   }
